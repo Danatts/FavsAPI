3ef06923c1a996bc04b4179307c8e211a76ce81f
Daniel <danielatts97@hotmail.com>
2022-05-08 12:11:57 -0500

    connected to database

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const configExpress = require('./config/express');
+const connectDB = require('./config/database');
 
 // Configuration
 configExpress(app);
+connectDB(process.env.MONGO_DB_URI);
 
 app.get('/', (req, res) => {
-  res.json('Favs API');
+  res.send('Favs API');
 });
 
 module.exports = { app };
---
+++ config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connectDB = async (uri) => {
+  try {
+    await mongoose.connect(uri);
+    console.log('Connected to database');
+  } catch(err) {
+    console.log(err);
+  };
+};
+
+module.exports = connectDB;
