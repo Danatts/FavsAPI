5a38a5be021f81d2183acf10da5a6a671b58fe79
Daniel <danielatts97@hotmail.com>
2022-05-08 11:40:59 -0500

    inital config

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+// Dev variables
 if (process.env.NODE_ENV === 'development'){
   require('dotenv').config();
 };
 
+// APP
 const { app } = require('./app');
 
+// Run server
 const port = process.env.PORT || 8080;
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
 })
