99477a180b3e3ce49ba1a39df3d95f79356e0618
Daniel <danielatts97@hotmail.com>
2022-05-08 11:40:59 -0500

    inital config

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const configExpress = require('./config/express');
+
+// Configuration
+configExpress(app);
 
 app.get('/', (req, res) => {
   res.json('Favs API');
