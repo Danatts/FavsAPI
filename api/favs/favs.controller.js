13ebd8367248f9141fb1973cb8cab4941ffe42d3
Daniel <danielatts97@hotmail.com>
2022-05-09 12:20:23 -0500

    create favs crud

--- api/favs/favs.controller.js
+++ api/favs/favs.controller.js
@@ -1,17 +1,85 @@
 const {
+  getAllFavs,
+  getFavById,
   createFav,
+  deleteFavById,
+  updateFavById,
 } = require('./favs.service');
 
 /**
- * ! Route: POST api/fav
- * ! Desc: Post a new favorite item
+ * ! Route: GET api/favs
+ * ! Desc: Get all favorites lists
  * ! Access: Private
+ */
+const handleGetAllFavs = async (req, res) => {
+  try {
+    const response = await getAllFavs();
+    res.status(200).json(response);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: 'Error' });
+  }
+};
+
+/**
+ * ! Route: GET api/favs/:id
+ * ! Desc: Get a single favorites list by id
+ * ! Access: Private
+ */
+const handleGetFavById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const response = await getFavById(id);
+    res.status(200).json(response);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: 'Error' });
+  }
+};
+
+/**
+ * ! Route: POST api/favs
+ * ! Desc: Post a new favorite list
+ * ! Access: Public
  */
 const handleCreateFav = async (req, res) => {
   const fav = req.body;
   try {
-    await createFav(fav);
-    res.status(201).json(fav);
+    const response = await createFav(fav);
+    res.status(201).json(response);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: 'Error' });
+  }
+};
+
+/**
+ * ! Route: DELETE api/favs/:id
+ * ! Desc: Delete a single favorites list by id
+ * ! Access: Private
+ */
+const handleDeleteFavById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await deleteFavById(id);
+    res.status(200).json({ message: 'List deleted successfully' });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: 'Error' });
+  }
+};
+
+/**
+ * ! Route: PATCH api/favs/:id
+ * ! Desc: Update a single favorites list by id
+ * ! Access: Private
+ */
+const handleUpdateFavById = async (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  try {
+    const response = await updateFavById(id, body);
+    res.status(200).json(response);
   } catch (err) {
     console.log(err);
     res.status(400).json({ message: 'Error' });
@@ -19,5 +87,9 @@ const handleCreateFav = async (req, res) => {
 };
 
 module.exports = {
+  handleGetAllFavs,
+  handleGetFavById,
   handleCreateFav,
+  handleDeleteFavById,
+  handleUpdateFavById,
 };
--- api/favs/favs.service.js
+++ api/favs/favs.service.js
@@ -1,7 +1,29 @@
 const FavModel = require('./favs.model');
 
+const getAllFavs = () => {
+  return FavModel.find({});
+};
+
+const getFavById = (id) => {
+  return FavModel.findById(id);
+};
+
 const createFav = (fav) => {
   return FavModel.create(fav);
 };
 
-module.exports = { createFav };
+const deleteFavById = (id) => {
+  return FavModel.findByIdAndDelete(id);
+};
+
+const updateFavById = (id, body) => {
+  return FavModel.findByIdAndUpdate(id, body, { new: true });
+};
+
+module.exports = {
+  getAllFavs,
+  getFavById,
+  createFav,
+  deleteFavById,
+  updateFavById,
+};
